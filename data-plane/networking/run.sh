b761bbd750f5b36323315201ecf79d381a055765
Raul Sevilla <rsevilla@redhat.com>
2024-01-10 12:27:23 +0100

    run.sh
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- data-plane/networking/run.sh
+++ data-plane/networking/run.sh
@@ -1,1 +1,11 @@
-curl -L https://github.com/cloud-bulldozer/k8s-netperf/releases/download/v0.1.20/k8s-netperf_Linux_v0.1.20_x86_64.tar.gz | tar xzf - k8s-netperf
+SIDECAR=${SIDECAR:?}
+curl -sSL https://github.com/cloud-bulldozer/k8s-netperf/releases/download/v0.1.20/k8s-netperf_Linux_v0.1.20_x86_64.tar.gz | tar xzf - k8s-netperf
+oc create ns netperf 2>/dev/null
+if [[ ${SIDECAR} == "true" ]]; then
+  echo "Adding istio-injection=enabled label to ns"
+  oc label ns netperf istio-injection=enabled --overwrite
+fi
+oc create sa netperf -n netperf
+oc adm policy add-scc-to-user hostnetwork -z netperf -n netperf
+./k8s-netperf --all --config ${1} --search ${2}
+oc delete ns netperf
