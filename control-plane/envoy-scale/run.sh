0834f40e1f91f185c14a5e7a2b85ebbd69333e7c
Raul Sevilla <rsevilla@redhat.com>
2024-02-02 12:07:44 +0100

    Bump kube-burner-ocp
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- control-plane/envoy-scale/run.sh
+++ control-plane/envoy-scale/run.sh
@@ -1,13 +1,15 @@
-KUBE_BURNER_VERSION=1.0.0
+KUBE_BURNER_VERSION=1.1.0
 PROMETHEUS_HOST=https://$(oc get route -n openshift-monitoring prometheus-k8s -o go-template="{{.spec.host}}")
 PROMETHEUS_ISTIO_HOST=https://$(oc get route -n istio-system prometheus -o go-template="{{.spec.host}}")
 PROMETHEUS_PASSWORD=$(oc get secret/htpasswd -n istio-system -o go-template="{{.data.rawPassword|base64decode}}")
 TOKEN=$(oc sa new-token -n openshift-monitoring prometheus-k8s)
 
-export PROMETHEUS_HOST PROMETHEUS_ISTIO_HOST PROMETHEUS_HOST TOKEN
+export PROMETHEUS_HOST PROMETHEUS_ISTIO_HOST PROMETHEUS_PASSWORD TOKEN
 
 if [[ ! -f /tmp/kube-burner-ocp ]]; then
   curl -sSL https://github.com/kube-burner/kube-burner-ocp/releases/download/v${KUBE_BURNER_VERSION}/kube-burner-ocp-V${KUBE_BURNER_VERSION}-linux-x86_64.tar.gz | tar xz -C /tmp/ kube-burner-ocp
 fi
 
-/tmp/kube-burner-ocp cluster-density-v2 ${@} --iterations=1
+cmd="/tmp/kube-burner-ocp cluster-density-v2 ${@} --iterations=1"
+echo ${cmd}
+exec ${cmd}
