b5941cc6ec42c29fc32e324066ea05a23fcf8a1e
Raul Sevilla <rsevilla@redhat.com>
2024-02-20 11:14:19 +0100

    Bash options
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- control-plane/run.sh
+++ control-plane/run.sh
@@ -1,3 +1,8 @@
+#!/bin/bash -e
+
+set -e
+set -o pipefail
+
 KUBE_BURNER_VERSION=1.1.0
 KUBE_DIR=${KUBE_DIR:-/tmp}
 KUBE_BURNER_URL="https://github.com/kube-burner/kube-burner-ocp/releases/download/v${KUBE_BURNER_VERSION}/kube-burner-ocp-V${KUBE_BURNER_VERSION}-linux-x86_64.tar.gz"
@@ -18,16 +23,16 @@ if [[ ! -f /tmp/kube-burner-ocp ]]; then
 fi
 
 
-cmd="/tmp/kube-burner-ocp ${WORKLOAD} ${EXTRA_FLAGS}"
-# If ES_SERVER is specified
-if [[ -n ${ES_SERVER} ]]; then
-  cmd+=" --es-server=${ES_SERVER} --es-index=${ES_INDEX}"
-fi
-
 if [[ ${WORKLOAD} =~ cluster-density-sm|envoy-scale ]]; then
   WORKLOAD="cluster-density-v2"
 elif [[ ${WORKLOAD} == "node-density-sm" ]]; then
   WORKLOAD="node-density"
+fi
+
+cmd="/tmp/kube-burner-ocp ${WORKLOAD} ${EXTRA_FLAGS}"
+# If ES_SERVER is specified
+if [[ -n ${ES_SERVER} ]]; then
+  cmd+=" --es-server=${ES_SERVER} --es-index=${ES_INDEX}"
 fi
 
 echo ${cmd}
