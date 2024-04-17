95368d229251c5553ed3c08b32093941f7fe0086
Raul Sevilla <rsevilla@redhat.com>
2024-04-17 12:23:29 +0200

    Run ambient for networking
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- data-plane/networking/run-ambient.sh
+++ data-plane/networking/run-ambient.sh
@@ -1,16 +1,12 @@
 NETPERF_VERSION=0.1.21
-SIDECAR=${SIDECAR:?}
 curl -sSL https://github.com/cloud-bulldozer/k8s-netperf/releases/download/v${NETPERF_VERSION}/k8s-netperf_Linux_v${NETPERF_VERSION}_x86_64.tar.gz | tar xzf - k8s-netperf
 oc create ns netperf 2>/dev/null
-if [[ ${SIDECAR} == "true" ]]; then
-  echo "Adding istio-injection=enabled label to ns"
-  oc label ns netperf istio-injection=enabled --overwrite
-fi
+oc label namespace netperf istio.io/dataplane-mode=ambient
 oc create sa netperf -n netperf
 oc adm policy add-scc-to-user hostnetwork -z netperf -n netperf
 if [[ $2 != "" ]]; then
-  ./k8s-netperf --all --config ${1} --search ${2} --clean=false
+  ./k8s-netperf --clean=false --config ${1} --search ${2}
 else
-  ./k8s-netperf --all --config ${1} --clean=false
+  ./k8s-netperf --clean=false --config ${1}
 fi
 oc delete ns netperf
