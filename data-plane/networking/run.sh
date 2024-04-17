d6558e18404007d1a63004c4396a7f7b0558d458
Raul Sevilla <rsevilla@redhat.com>
2024-04-17 12:17:57 +0200

    Add tests with 2 streams
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- data-plane/networking/run.sh
+++ data-plane/networking/run.sh
@@ -1,5 +1,6 @@
+NETPERF_VERSION=0.1.21
 SIDECAR=${SIDECAR:?}
-curl -sSL https://github.com/cloud-bulldozer/k8s-netperf/releases/download/v0.1.20/k8s-netperf_Linux_v0.1.20_x86_64.tar.gz | tar xzf - k8s-netperf
+curl -sSL https://github.com/cloud-bulldozer/k8s-netperf/releases/download/v${NETPERF_VERSION}/k8s-netperf_Linux_v${NETPERF_VERSION}_x86_64.tar.gz | tar xzf - k8s-netperf
 oc create ns netperf 2>/dev/null
 if [[ ${SIDECAR} == "true" ]]; then
   echo "Adding istio-injection=enabled label to ns"
